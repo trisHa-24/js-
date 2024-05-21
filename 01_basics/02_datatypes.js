6d76a467a7a8e08d45e151f79dd7acf67b2b6d25
Trisha <154320949+trisHa-24@users.noreply.github.com>
2024-05-21 03:39:19 +0000

    21-may

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+"use strict"; //treat all js code as newer version
+// alert(3+3) // we are using nodejs, not browser
+
+let name = "trisha"
+let age = 22;
+let isLoggedIn = true
+
+console.log(typeof null);
+console.log(typeof undefined);
--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -2,8 +2,12 @@ const accountId = 123456
 let accountEmail = "trisha@google.com"
 var accountPassword = "742266"
 accountCity = "berhampore"
-
+let accountState;
 //accountId = 2;
 
 console.log(accountId);
-accountPassword = "";
+accountPassword = "22222";
+accountCity = "kolkata";
+accountEmail= "mondal@gmail.com";
+
+console.table([accountEmail,accountPassword, accountCity, accountState]);
