eaa4bbd8bf568cb593ebb4eb0f314da7877e4883
Trisha <154320949+trisHa-24@users.noreply.github.com>
2024-05-21 03:04:59 +0000

    chabged fontsize

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 123456
+let accountEmail = "trisha@google.com"
+var accountPassword = "742266"
+accountCity = "berhampore"
+
+//accountId = 2;
+
+console.log(accountId);
+accountPassword = "";
