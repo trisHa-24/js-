48a867a62689030418615b010ab4a089e9d495bc
Trisha <154320949+trisHa-24@users.noreply.github.com>
2024-05-28 17:25:28 +0000

    06

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,20 @@
+const score=400;
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const num = 123.89966;
+console.log(num.toPrecision(4));
+const num1 =1123.899;
+console.log(num1.toPrecision(3));
+
+const hundreds = 1000000;
+console.log(hundreds.toLocaleString('en-IN'));
+
+///..........maths..........
+console.log(Math);
+console.log(Math.random()*10);
