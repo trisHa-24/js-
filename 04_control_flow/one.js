c9f769c529eb481c621929015d2a983af1e8fe22
Trisha <154320949+trisHa-24@users.noreply.github.com>
2024-06-04 11:25:57 +0000

    c

---
+++ 04_control_flow/one.js
@@ -0,0 +1,21 @@
+//if
+
+// if(2 == '2'){
+//     console.log("equal");
+// }
+// == do not check datatypr but === stricktly checks
+
+// if(2==='2'){
+//    console.log("equal");
+// }
+// else{
+//  console.log("not equal");
+// }
+
+const userLoggedIn = true
+const debitCard = true
+
+if(userLoggedIn && debitCard){
+    console.log("Allow to buy course");
+}
+
